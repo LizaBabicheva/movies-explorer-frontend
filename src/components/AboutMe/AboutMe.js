eec0260783547c52679b33ace94a74044b48be3e
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-07 16:43:15 +0700

    install react router

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe() {
       <div className='about-me__profile'>
         <h3 className='about-me__name'>Елизавета</h3>
         <p className='about-me__occupation'>Фронтенд-разработчик, 31 год</p>
-        <p className='about-me__info'>"Я родился в Москве в 70-м на краю города..."</p>
+        <p className='about-me__info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         <a className='about-me__link'>Github</a>
         <img className='about-me__photo' alt='Фотография'></img>
       </div>
