a5547a3ba71f4b160a927c74b132298110a35dc5
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-08 12:39:21 +0700

    add mobile version to AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,35 +1,39 @@
 import './AboutMe.css';
-import linkIcon from '../../images/link-icon.svg';
+// ЗАМЕНИТЬ ССЫЛКИ
 
 function AboutMe() {
   return (
     <section className="about-me">
       <h2 className='main__section-title'>Студент</h2>
-      
+
       <div className='about-me__profile'>
         <h3 className='about-me__name'>Елизавета</h3>
         <p className='about-me__occupation'>Фронтенд-разработчик, 31 год</p>
-        <p className='about-me__info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <a className='about-me__link'>Github</a>
+        <p className='about-me__info'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+        <a className='about-me__link'
+          href='https://github.com/LizaBabicheva'>Github</a>
         <img className='about-me__photo' alt='Фотография'></img>
       </div>
 
       <div className='about-me__portfolio'>
         <h4 className='about-me__portfolio-title'>Портфолио</h4>
         <ul className='about-me__portfolio-links'>
-          <li>
-            <a className='about-me__portfolio-link'>Статичный сайт
-              <img className='about-me__link-icon' src={linkIcon} alt='Перейти на сайт'></img>
+          <li className='about-me__list-item'>
+            <a className='about-me__portfolio-link'
+              href='https://github.com/LizaBabicheva'>Статичный сайт
+              <span className='about-me__span'>&#8599;</span>
             </a>
           </li>
-          <li className='about-me__portfolio-link'>
-            <a className='about-me__portfolio-link'>Адаптивный сайт
-              <img className='about-me__link-icon' src={linkIcon} alt='Перейти на сайт'></img>
+          <li className='about-me__list-item'>
+            <a className='about-me__portfolio-link'
+              href='https://github.com/LizaBabicheva'>Адаптивный сайт
+              <span className='about-me__span'>&#8599;</span>
             </a>
           </li>
-          <li className='about-me__portfolio-link'>
-            <a className='about-me__portfolio-link'>Одностраничное приложение
-              <img className='about-me__link-icon' src={linkIcon} alt='Перейти на сайт'></img>
+          <li className='about-me__list-item'>
+            <a className='about-me__portfolio-link'
+              href='https://github.com/LizaBabicheva'>Одностраничное приложение
+              <span className='about-me__span'>&#8599;</span>
             </a>
           </li>
         </ul>
