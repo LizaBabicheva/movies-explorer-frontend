dc925fcb00e5a39cfa0984e171f90a3e607f0197
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-10 20:43:07 +0700

    create MovieCard component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,23 +1,77 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
+import MoviesCard from '../MoviesCard/MoviesCard';
+
+import aboutDesign from '../../images/pic_1.jpg';
+import hundredYears from '../../images/pic_2.jpg';
+import benksy from '../../images/pic_3.jpg';
+import basquiat from '../../images/pic_4.jpg';
+import running from '../../images/pic_5.jpg';
+import bookSellers from '../../images/pic_6.jpg';
+import aboutGermany from '../../images/pic_7.jpg';
+import gimmeDanger from '../../images/pic_8.jpg';
+import smallGirl from '../../images/pic_9.jpg';
+import beforeJump from '../../images/pic_10.jpg';
+import pjHarvey from '../../images/pic_11.jpg';
+import soundArt from '../../images/pic_12.jpg';
 
 function Movies() {
   return (
     <section className="movies">
       <SearchForm />
-      <div className='movies-list'></div>
-        <ul>
-          <li className='movie-card'>
-            <img className='movie-card__img' alt='' src=''></img>
-            <div className='movie-card__description'>
-              <h2 className='movie-card__title'></h2>
-              <p className='movie-card__duration'></p>
-              <button className='movie-card__like-button' type="button" aria-label="Нравится"></button>
-            </div>
-          </li>
-        </ul>
+      <ul className='movies-list'>
 
-        <button className='movies__more-button'>Ещё</button>
+        <MoviesCard
+          title='33 слова о дизайне'
+          image={aboutDesign}
+          duration='1ч 47м' />
+        <MoviesCard
+          title='Киноальманах «100 лет дизайна»'
+          image={hundredYears}
+          duration='1ч 3м' />
+        <MoviesCard
+          title='В погоне за Бенкси'
+          image={benksy}
+          duration='1ч 42м' />
+        <MoviesCard
+          title='Баския: Взрыв реальности'
+          image={basquiat}
+          duration='1ч 21м' />
+        <MoviesCard
+          title='Бег это свобода'
+          image={running}
+          duration='1ч 44м' />
+        <MoviesCard
+          title='Книготорговцы'
+          image={bookSellers}
+          duration='1ч 37м' />
+        <MoviesCard
+          title='Когда я думаю о Германии ночью'
+          image={aboutGermany}
+          duration='1ч 56м' />
+        <MoviesCard
+          title='Gimme Danger: История Игги и The Stooge...'
+          image={gimmeDanger}
+          duration='1ч 59м' />
+        <MoviesCard
+          title='Дженис: Маленькая девочка грустит'
+          image={smallGirl}
+          duration='1ч 42м' />
+        <MoviesCard
+          title='Соберись перед прыжком'
+          image={beforeJump}
+          duration='1ч 10м' />
+        <MoviesCard
+          title='Пи Джей Харви: A dog called money'
+          image={pjHarvey}
+          duration='1ч 4м' />
+        <MoviesCard
+          title='По волнам: Искусство звука в кино'
+          image={soundArt}
+          duration='1ч 7м' />
+
+      </ul>
+      <button className='movies__more-button' type='button' aria-label='Загрузить еще'>Ещё</button>
     </section>
   )
 }
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './MoviesCard.css';
 
-function MoviesCard() {
+function MoviesCard({ title, image, duration }) {
   return (
     <li className='movie-card'>
-      <img className='movie-card__img' alt='' src=''></img>
+      <img className='movie-card__img' alt={`Постер к фильму ${title}`} src={image}></img>
       <div className='movie-card__description'>
-        <h2 className='movie-card__title'></h2>
-        <p className='movie-card__duration'></p>
-        <button className='movie-card__like-button' type="button" aria-label="Нравится"></button>
+        <h2 className='movie-card__title'>{title}</h2>
+        <button className='movie-card__like-button' type='button' aria-label='Нравится'></button>
+        <p className='movie-card__duration'>{duration}</p>
       </div>
     </li>
   )
 }
-  
+
 export default MoviesCard;
