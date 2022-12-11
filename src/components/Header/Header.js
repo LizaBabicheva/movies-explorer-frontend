1887e041c338f9e1259d6991dd74e5ef90e4fd67
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-11 14:03:06 +0700

    refactor header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,26 @@ import Navigation from '../Navigation/Navigation';
 function Header() {
   return (
     <header className='header'>
-      <Link
-        className='header__logo'
-        to='/'>
-        <img src={logo} alt='Лого'></img>
-      </Link>
+      <Route exact path='/'>
+
+
+        <Link className='header__logo' to='/'>
+          <img src={logo} alt='Лого'></img>
+        </Link>
+
+
 
       {/* <Navigation /> */}
 
       <nav className='header__menu'>
         <Link className='header__menu-link' to='/signup'>Регистрация</Link>
-        <button className='header__menu-button'>Войти
-        </button>
+        <Link className='header__menu-link header__menu-link_type_button' to='/signin'>Войти</Link>
       </nav>
+      </Route>
+
+      <Route path='/movies'>
+        <Navigation />
+      </Route>
 
       {/* <button className='header__profile-button'>Аккаунт
         <div className='header__profile-icon'></div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,8 +4,11 @@ import './Navigation.css';
 function Navigation() {
   return (
     <nav className='navigation'>
-      <Link className='navigation__link' to=''>Фильмы</Link>
-      <Link className='navigation__link' to=''>Сохранённые фильмы</Link>
+      <Link className='navigation__link' to='/movies'>Фильмы</Link>
+      <Link className='navigation__link' to='/saved-movies'>Сохранённые фильмы</Link>
+      <Link className='navigation__link navigation__link_type_profile-button' to='/profile'>Аккаунт
+        <div className='navigation__profile-icon'></div>
+      </Link>
     </nav>
   )
 }
