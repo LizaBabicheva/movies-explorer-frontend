c2a7d6b67f7ce33f5ac51d929b45a377f0f01391
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-16 19:09:33 +0700

    refactor Register css

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,3 @@
-// import React from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
 import logo from '../../images/logo.svg';
@@ -6,16 +5,19 @@ import logo from '../../images/logo.svg';
 function Register() {
   return (
     <section className='register'>
-      <Link className='register__logo' to='/'><img src={logo} alt='Лого'></img></Link>
+      <Link className='header__logo header__menu-link' to='/'><img src={logo} alt='Лого'></img></Link>
       <h2 className='register__title'>Добро пожаловать!</h2>
 
       <form className='register__form'>
-      <label
+        <label
           className='register__label'
           htmlFor='name'>Имя</label>
-        <input 
+        <input
           className='register__input'
-          type="text"
+          type='text'
+          minLength='2'
+          maxLength='30'
+          required
           id='name'
           name='name'>
         </input>
@@ -24,9 +26,10 @@ function Register() {
         <label
           className='register__label'
           htmlFor='email'>E-mail</label>
-        <input 
+        <input
           className='register__input'
-          type="email"
+          type='email'
+          required
           id='email'
           name='email'>
         </input>
@@ -36,20 +39,24 @@ function Register() {
           className='register__label'
           htmlFor='password'>Пароль</label>
         <input
-          className='register__input'
+          className='register__input register__input_type_error'
           type='password'
+          required
           id='password'
           name='password'>
         </input>
         <span className='register__error'>Что-то пошло не так...</span>
-       
-        <button className='register__submit'>Войти</button>
-        </form>
 
-        <div className='register__login'>
-          <p className='register__login-text'>Ещё не зарегистрированы?&nbsp;</p>
-          <Link to='/signin' className='register__login-link'>Регистрация</Link>
-        </div>
+        <button
+          className='register__submit'
+          type='submit'>Зарегистрироваться
+        </button>
+      </form>
+
+      <div className='register__login'>
+        <p className='register__login-text'>Уже зарегистрированы?&nbsp;</p>
+        <Link to='/signin' className='register__login-link'>Войти</Link>
+      </div>
     </section>
   )
 }
